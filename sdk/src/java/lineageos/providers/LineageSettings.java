dab399cbd84b92b5647750264ace416cb32d7bec
Oliver Scott <olivercscott@gmail.com>
2021-12-21 12:43:54 +0100

    Add preference key for toggleable tiles on keyguard, secure
    Change-Id: Ie6dce37070059c046b831541d2969bf9d92cce93

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -2930,6 +2930,17 @@ public final class LineageSettings {
 
         /** @hide */
         public static final Validator PANIC_IN_POWER_MENU_VALIDATOR = sBooleanValidator;
+
+        /**
+         * Whether user is allowed to interact with quick settings on lockscreen.
+         * @hide
+         */
+        public static final String QS_TILES_TOGGLEABLE_ON_LOCK_SCREEN =
+                "qs_tiles_toggleable_on_lock_screen";
+
+        /** @hide */
+        public static final Validator QS_TILES_TOGGLEABLE_ON_LOCK_SCREEN_VALIDATOR =
+                sBooleanValidator;
         // endregion
 
         /**
@@ -2954,6 +2965,8 @@ public final class LineageSettings {
             VALIDATORS.put(BERRY_BLACK_THEME, BERRY_BLACK_THEME_VALIDATOR);
             VALIDATORS.put(GESTURE_BACK_EXCLUDE_TOP, GESTURE_BACK_EXCLUDE_TOP_VALIDATOR);
             VALIDATORS.put(PANIC_IN_POWER_MENU, PANIC_IN_POWER_MENU_VALIDATOR);
+            VALIDATORS.put(QS_TILES_TOGGLEABLE_ON_LOCK_SCREEN,
+                    QS_TILES_TOGGLEABLE_ON_LOCK_SCREEN_VALIDATOR);
             VALIDATORS.put(TRUST_WARNINGS, TRUST_WARNINGS_VALIDATOR);
             VALIDATORS.put(VOLUME_PANEL_ON_LEFT, VOLUME_PANEL_ON_LEFT_VALIDATOR);
         }
