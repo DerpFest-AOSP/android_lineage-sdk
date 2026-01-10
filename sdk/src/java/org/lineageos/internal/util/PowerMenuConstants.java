8752866497bd4e14381a32f78eaf997d52c81c9f
NurKeinNeid <nurkeinneid@derpfest.org>
2026-01-10 20:40:16 +0100

    PowerMenuConstants: Add panic action to power menu options
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- sdk/src/java/org/lineageos/internal/util/PowerMenuConstants.java
+++ sdk/src/java/org/lineageos/internal/util/PowerMenuConstants.java
@@ -26,6 +26,7 @@ public class PowerMenuConstants {
     public static final String GLOBAL_ACTION_KEY_STANDBY = "standby";
     public static final String GLOBAL_ACTION_KEY_LOCK = "lock";
     public static final String GLOBAL_ACTION_KEY_RESTART_SYSTEMUI = "restart_systemui";
+    public static final String GLOBAL_ACTION_KEY_PANIC = "panic";
 
     /**
      * Advanced restart menu actions
@@ -50,6 +51,7 @@ public class PowerMenuConstants {
         GLOBAL_ACTION_KEY_ASSIST,
         GLOBAL_ACTION_KEY_DEVICECONTROLS,
         GLOBAL_ACTION_KEY_RESTART_SYSTEMUI,
+        GLOBAL_ACTION_KEY_PANIC,
         GLOBAL_ACTION_KEY_LOGOUT,
         GLOBAL_ACTION_KEY_SYSTEM_UPDATE,
         GLOBAL_ACTION_KEY_STANDBY,
